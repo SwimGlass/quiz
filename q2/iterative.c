75a4da68aa396ea7825a58c30986df6bcf85fb5c
SwimGlass <swimmingglasses@gmail.com>
2015-10-14 23:14:12 +0800

    Add iterative code for question with bit operation.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,46 @@
 /* FIXME: Implement! */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <err.h>
+#define BUFFERSIZE 1024
+char smallest_character(int num, int c)
+{
+    int tmp = num;
+    int checkbit = c - 97;
+    if( 1 & (tmp >> checkbit)){ //find character , output next
+        while((tmp & 1) == 0){ 
+            tmp = tmp >> 1;
+            checkbit++;
+        }
+        checkbit++;
+        return (checkbit+97);
+    }
+    else{// no find character , output min
+        while((tmp & 1)==0){
+            tmp = tmp >> 1;
+            checkbit++;
+        }
+        return (checkbit+97);
+    }
+}
 
 int main()
 {
+    FILE *fp = fopen("array.txt","r");
+    char buffer[BUFFERSIZE];
+    int i = 0,c,num=0;
+    if( fp == NULL) err(1,"Error:file open failure\n");
+    else{
+        while((c = fgetc(fp))!=EOF){
+            num |= 1 << ((int)c - 97);
+        }
+    }
+    printf("%d\n",num);
+    fclose(fp);
+    char temp = 'a';
+    printf("Output : %c\n",(char)smallest_character(num,temp));
     return 0;
 }
+
+
